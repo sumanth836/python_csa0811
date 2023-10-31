e5017513f44abdfe8f33f2ad87533ccfbb1af651
sumanth836 <149452984+sumanth836@users.noreply.github.com>
2023-10-30 22:38:56 -0500

    Create lower_upper_range.py

---
+++ lower_upper_range.py
@@ -0,0 +1,7 @@
+lower_range = int(input("Enter the lower range: "))
+upper_range = int(input("Enter the upper range: "))
+
+result_list = [(num, num**2) for num in range(lower_range, upper_range + 1)]
+
+
+print(result_list)
